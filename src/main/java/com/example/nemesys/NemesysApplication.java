60d75a695e194aa4a6f092a41e366cd84a0f13dc
Peter Salamun <peter.salamun@gmail.com>
2025-01-07 12:34:15 +0100

    Initial commit for NEMESYS application

---
+++ src/main/java/com/example/nemesys/NemesysApplication.java
@@ -0,0 +1,14 @@
+package com.example.nemesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemesysApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/nemesys/NemesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nemesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemesysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
