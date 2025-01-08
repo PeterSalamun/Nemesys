e1c77192dd573ac39b72f0840bdbae03dde2727a
Peter Salamun <peter.salamun@gmail.com>
2025-01-08 09:24:35 +0100

    add indicesCalculate controller

--- src/main/java/com/example/nemesys/controllers/BasicController.java
+++ src/main/java/com/example/nemesys/controllers/BasicController.java
@@ -1,13 +1,17 @@
 package com.example.nemesys.controllers;
 
+import com.example.nemesys.services.IndicesCalculation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api")
-public class basicController {
+public class BasicController {
+
+    @Autowired
+    private IndicesCalculation indicesCalculation;
 
     @GetMapping("/nemesys")
     public String indexPage() {
@@ -15,6 +19,6 @@ public class basicController {
     }
 
     @GetMapping("/nemesys/calculate")
-    public String indicesCalculate() {return "Hello from calculator";}
+    public String indicesCalculate() {return indicesCalculation.getIndxes();}
 
 }
