017144961a4fd995d5b725e2a4f060ca72ae30cd
Peter Salamun <peter.salamun@gmail.com>
2025-01-08 09:24:58 +0100

    create service IndicesCalculation

---
+++ src/main/java/com/example/nemesys/services/IndicesCalculation.java
@@ -0,0 +1,13 @@
+package com.example.nemesys.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndicesCalculation {
+
+    public String getIndxes() {
+        return "Service method getIndexes()";
+    }
+
+
+}
